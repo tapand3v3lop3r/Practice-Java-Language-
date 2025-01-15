e1c81da3055b8fe720102ccb1034ba936e91e744
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-01-15 10:31:31 +0530

    Create 0001_FirstProgram.java

---
+++ 0001_FirstProgram.java
@@ -0,0 +1,20 @@
+public class FirstProgram 
+{
+      public static void main(String[] args) 
+      {
+            //METHOD - 1 
+            System.out.println("Hey, my first program in java!!!");
+            
+            // METHOD - 2
+            System.out.print("Hey, my first program in java");
+      }
+}
+
+
+
+**LEARNINGS**
+```
+1. learned about comments.
+2. learned the difference between 'print' and 'println'.
+```
+
