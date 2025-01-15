a98a95eb044243b6df44f41df533d1ad432e81b0
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-01-15 11:02:44 +0530

    Create 0002_StringsProgram.java

---
+++ 0002_StringsProgram.java
@@ -0,0 +1,29 @@
+public class StringsProgram {
+      public static void main(String[] args){
+            System.out.println("Today I will be learning about strings...!!!");
+
+            String username = "Tapan Kumar Padhi";
+
+            System.out.println("I know C, Python and Java !!!");
+            System.out.println(username);
+
+            System.out.println("Anything inside these inverted commas is treated as a string, be it any number or anything...");
+            System.out.println("Next we will be learning about escape sequence characters...");
+
+            System.out.println("My _____ is to become a Data _______.");
+
+            String first = "dream";
+            String second = "Scientist";
+
+            System.out.println("My "+ first +"is to become a Data "+ second+".");
+            
+      }
+}
+
+
+LEARNINGS
+```
+1. learned about strings 
+2. learned how to declare a string and print it
+3. learned how to use strings and sentences with conacatenation 
+```
