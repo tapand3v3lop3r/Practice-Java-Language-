8f0c07fb4a7db003798bdb1c53f3e2b53601474c
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-01-20 17:32:25 +0530

    Create 0003_LongDoubleFloat.java

---
+++ 0003_LongDoubleFloat.java
@@ -0,0 +1,14 @@
+public class LongDoubleFloat {
+    public static void main(String[] args){
+
+        int a = 9;
+        float b = 9.0F;
+        long c = 99999999999999999L;
+        double d = 1090932080238D;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
