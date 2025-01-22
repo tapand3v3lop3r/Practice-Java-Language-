dacb86dec25dc2828bf808fe9ab8c32f3aa5f5ae
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-01-22 11:11:35 +0530

    Create 0007_ZombieKiller.java

---
+++ 0007_ZombieKiller.java
@@ -0,0 +1,26 @@
+public class ZombieKiller {
+    public static void main(String[] args){
+
+        System.out.println("Hey there!!! ");
+        System.out.println("Welcome to our Zombie Killer Program");
+        System.out.println("     ");
+        // Storing of weapons
+        String[] WeaponBag = {"Shotgun", "Assault Rifle", "Sniper", "Grenade", "Extinguisher"};
+        String[] Zombies = {"short-range-zombie", "mid-range-zombie", "long-range-zombie", "fat-and-huge-zombie", "weak-zombie"};
+
+        System.out.println("These are the weapons for you to fight!!!");
+        System.out.println(WeaponBag[0]);
+        System.out.println(WeaponBag[1]);
+        System.out.println(WeaponBag[2]);
+        System.out.println(WeaponBag[3]);
+        System.out.println(WeaponBag[4]);
+        System.out.println("     ");
+
+        System.out.println("And these are your zombies....Go and kill them!!!");
+        System.out.println(Zombies[0]);
+        System.out.println(Zombies[1]);
+        System.out.println(Zombies[2]);
+        System.out.println(Zombies[3]);
+        System.out.println(Zombies[4]);
+    }
+}
