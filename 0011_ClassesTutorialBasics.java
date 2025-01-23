c6add37e3d8d11d9a4ec6147aef5b2d4fbc00ac2
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-01-23 15:18:21 +0530

    Create 0011_ClassesTutorialBasics.java

---
+++ 0011_ClassesTutorialBasics.java
@@ -0,0 +1,10 @@
+public class ClassesTutorialBasic {
+    int x = 5;
+
+    public static void main(String[] args){
+        ClassesTutorialBasic myobj = new ClassesTutorialBasic();
+        System.out.println(myobj.x);
+    }
+}
+
+
