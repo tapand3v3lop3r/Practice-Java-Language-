881548b7e29c84b334ba5c2d148b51d41c6f3ae1
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-01-25 16:23:56 +0530

    Create 0002_StdinStdout_level1.java

---
+++ HackerRank_Problems/0002_StdinStdout_level1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);        
+    }
+}
